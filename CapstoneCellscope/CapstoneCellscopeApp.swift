951ed425b14217a179e9ddce614835a4d6817540
Oni Castillo-Ruiz <oni.castillo@upr.edu>
2021-05-15 18:15:10 -0400

    Initial Commit

---
+++ CapstoneCellscope/CapstoneCellscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneCellscopeApp.swift
+//  CapstoneCellscope
+//
+//  Created by Oni on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneCellscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
